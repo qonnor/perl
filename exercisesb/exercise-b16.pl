2830585e0de806891327168fe36dfb8ae9335651
Connor Pujals <pujals.connor@gmail.com>
2014-05-01 15:51:05 -0700

    atempt to answer final problem!  Please enter the commit message for your changes. Lines starting

--- exercisesb/exercise-b16.pl
+++ exercisesb/exercise-b16.pl
@@ -1,21 +1,24 @@
 #!/usr/bin/perl
 
+use strict;
 use warnings;
-
-#enter a sequence of numbers between 1 and 20 or any uppercase or lowercase
-#letter separated by spaces. 
-#you must then end the sequence with a 'q' or 'Q' followed by a space.
-
-#no regular expressions or arrays for #16 and use arrays for #17 (same problem)
 
 
 print "Please enter a sequence of numbers or letters: ";
+chomp($input = <STDIN>);
+$max = 20;
+$offset = (a .. z);
 
-#must have letters ignored and the sum of digits printed        
- 
-while ($input [1-20] )  
+for ($input <= $max) {
+
+
+
+
+@count = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20); 
+
+$add = $input ++;
+
+while ($input == @count)  
 {
-print "The sum of this input is:  #SUM OF INPUT #ignore letters   ";
-$input = <STDIN>;
-chomp $input;
+print "The sum of this input is: $add\n";
 }
